4a598615bc4811aaab3a49d9e9e5f0dcfefe6bdb
weesx <weslei002@gmail.com>
2025-01-30 16:48:14 -0300

    inicializacao django

---
+++ ecommerce/loja/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', homepage)
+]
+
+
+git config --global user.name "John Doe"
+$ git config --global user.email johndoe@example.com
