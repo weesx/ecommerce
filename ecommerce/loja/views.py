cc2b74ebec7825c4fa1e34ceb141a584126a4ef8
weesx <weslei002@gmail.com>
2025-01-30 22:15:40 -0300

    imagens e base e htmls

--- ecommerce/loja/views.py
+++ ecommerce/loja/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
-def homepage():
-    pass
+def homepage(request):
+    return render(request, 'homepage.html')
+
+def loja(request):
+    return render(request, 'loja.html')
+
+def carrinho(request):
+    return render(request, 'carrinho.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def minha_conta(request):
+    return render(request, 'usuario/minha_conta.html')
+
+def login(request):
+    return render(request, 'usuario/login.html')
 
